b7b2d8034c227ca6bf2285bb8c8c63c5f0bae507
Unknown <joel@45press.com>
2018-12-12 23:20:57 -0500

    JSON instagram feed added

---
+++ js/jq.js
@@ -0,0 +1,14 @@
+var jsonURL = "https://www.juicer.io/api/feeds/thalia-instagram.json";
+$.getJSON(jsonURL, function (json)
+{
+    var imgList= "";
+    var i = 0;
+    $.each(json.posts.items, function () {
+        if (++i > 9) {
+            return false;
+    }
+        imgList += '<li><img src= "' + this.image + '"></li>';
+    });
+
+    $('#images').append(imgList);
+});
