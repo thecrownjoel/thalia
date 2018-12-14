3633a991de567561640df758f00186fa60174423
Unknown <joel@45press.com>
2018-12-13 23:50:18 -0500

    Adding links to instagram feed

--- js/jq.js
+++ js/jq.js
@@ -7,8 +7,11 @@ $.getJSON(jsonURL, function (json)
         if (++i > 9) {
             return false;
     }
-    console.log(this);
-        imgList += '<li><img src= "' + this.image + '"></li>';
+    var url = this.full_url;
+    var img = this.image;
+
+    console.log(this.full_url);
+        imgList += '<div><a href="' + url + '" target="_blank"><img src= "' + img + '"></a></div>';
     });
 
     $('#images').append(imgList);
